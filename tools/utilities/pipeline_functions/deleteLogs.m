ca182c7a2e60ddf33fa3547f6ddc3a0ea5f1204d
JemimaSadhika <43720203+JemimaSadhika@users.noreply.github.com>
2022-04-12 21:43:26 +0530

    Removing utilities to add the updated utilities

--- tools/utilities/pipeline_functions/deleteLogs.m
+++
@@ -1,4 +0,0 @@
-% Copyright 2021 The MathWorks, Inc.
-% This script deletes all the logs generated at every stage of the pipeline
-load(fullfile('Code','logsPath.mat'),'path');
-rmdir(fullfile(path,'logs'),'s')
